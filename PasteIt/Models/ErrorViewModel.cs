7dfe8ef257b5088c765fe47db198edd24c3d16fa
lee.jonghan <lee.jonghan@nexon.com>
2018-01-05 18:27:34 +0900

    プロジェクト ファイルを追加します。

---
+++ PasteIt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PasteIt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
